13b16e4150dcd8d8edb5800de7f836264dd452d1
Nukem <Nukem@outlook.com>
2017-08-18 02:55:19 -0400

    Remove unneeded exports for cl_hook/ds_hook

--- components/wic_cl_hook/dbghelp.cpp
+++
@@ -1,6 +0,0 @@
-#include "dbghelp.h"
-
-BOOL WINAPI MiniDumpWriteDump(HANDLE hProcess, DWORD ProcessId, HANDLE hFile, int DumpType, void *ExceptionParam, void *UserStreamParam, void *CallbackParam)
-{
-	return FALSE;
-}
--- components/wic_cl_hook/dbghelp.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
-
-extern "C" __declspec(dllexport) BOOL WINAPI MiniDumpWriteDump(HANDLE hProcess, DWORD ProcessId, HANDLE hFile, int DumpType, void *ExceptionParam, void *UserStreamParam, void *CallbackParam);

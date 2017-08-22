aec4b65b2ff1840a0318097b0587afaa440ea218
Nukem <Nukem@outlook.com>
2017-08-22 18:48:43 -0400

    CL: Allow cdkeys to be used interchangeably (mixed WIC/WIC:SA versions)

--- components/wic_cl_hook/src/mmassgate/stdafx.h
+++ components/wic_cl_hook/src/mmassgate/stdafx.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#include "MMG_Protocols.h"
+#include "MMG_BitStream.h"
 #include "MMG_CryptoHash.h"
 #include "MMG_ICipher.h"
 #include "MMG_BlockTEA.h"
+#include "MMG_Tiger.h"
 #include "MMG_CdKey.h"
+#include "MMG_Protocols.h"

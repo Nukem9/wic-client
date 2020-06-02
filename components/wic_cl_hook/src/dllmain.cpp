d04dd56a31cfb852909361f33fd2cf33f7013a42
Nukem <Nukem@outlook.com>
2020-06-02 05:19:51 -0400

    CL: Minor refactor, add explicit version check

--- components/wic_cl_hook/src/dllmain.cpp
+++ components/wic_cl_hook/src/dllmain.cpp
@@ -1,42 +1,26 @@
 #include "stdafx.h"
 
-void WIC_WriteConsole(const char *aString, ...)
+void WriteEncryptionKeys(void *accountQuery, MN_WriteMessage *aMessage)
 {
-	char buffer[2048];
-	va_list va;
-
-	va_start(va, aString);
-	_vsnprintf_s(buffer, _TRUNCATE, aString, va);
-	va_end(va);
-
-	// EX3D_Console::StuffText
-	if (*(voidptr_t *)0x00E147A4)
-		((void (__cdecl *)(voidptr_t, const char *, ...))0x00AFDB90)(*(voidptr_t *)0x00E147A4, buffer);
-
-	printf("%s\n", buffer);
-}
-
-void hook_wrapper(void *accountQuery, MN_WriteMessage *aMessage)
-{
-	MMG_BlockTEA *myCipher = *(MMG_BlockTEA **)((DWORD)accountQuery + 0x4EA);
+	auto myCipher = *(MMG_BlockTEA **)((uintptr_t)accountQuery + 0x4EA);
 
 	for(int i = 0; i < ARRAYSIZE(myCipher->myEncryptionKey); i++)
 		aMessage->WriteUInt(myCipher->myEncryptionKey[i]);
 }
 
-void __declspec(naked) hook()
+void __declspec(naked) hk_MMG_AccountProtocol__Query__ToStream()
 {
 	__asm
 	{
 		push esi
 		push ebx
-		call hook_wrapper
-		add esp, 8h
+		call WriteEncryptionKeys
+		add esp, 0x8
 
-		mov cl, byte ptr ds:[0DFDA6Ah]
+		mov cl, byte ptr ds:[0x0DFDA6A]
 		xor eax, eax
 
-		push 00BD1C7Bh
+		push 0x00BD1C7B
 		retn
 	}
 }
@@ -56,43 +40,49 @@ struct hostent *PASCAL hk_gethostbyname(const char *name)
 
 BOOL Wic_HookInit(HMODULE hModule, DWORD ul_reason_for_call)
 {
-#if 0
-	if (AllocConsole())
+	if (_stricmp((const char *)0x00D2738C, "henrik.davidsson/MSV-BUILD-04 at 10:51:42 on Jun 10 2009.\n") != 0)
 	{
-		freopen("CONOUT$", "w", stdout);
-		freopen("CONOUT$", "w", stderr);
+		MessageBoxA(nullptr, "Unknown game version detected. Version 1.0.1.1 is required.", "Error", MB_ICONERROR);
+		return FALSE;
 	}
-#endif
 
-	// Set the protocol version
 	//MMG_Protocols::MassgateProtocolVersion = 150;
 
+	//
 	// Always enable the console
-	PatchMemory(0x00B31A16, (PBYTE)"\xEB", 1);
+	//
+	PatchMemory(0x00B31A16, (uint8_t *)"\xEB", 1);
 
+	//
 	// Copy MC_Debug::DebugMessage strings directly to the console output
-	Detours::X86::DetourFunction((PBYTE)0x00A01520, (PBYTE)&WIC_WriteConsole);
+	//
+	Detours::X86::DetourFunction((uint8_t *)0x00A01520, (uint8_t *)&EX3D_Console::StuffTextStatic);
 
+	//
 	// Write MMG_AccountProtocol cipher keys directly after EncryptionKeySequenceNumber in message packets
-	Detours::X86::DetourFunction((PBYTE)0x00BD1C73, (PBYTE)&hook);
+	//
+	Detours::X86::DetourFunction((uint8_t *)0x00BD1C73, (uint8_t *)&hk_MMG_AccountProtocol__Query__ToStream);
 
+	//
 	// Register custom commands to show the ingame debug menu
+	//
 	Detours::X86::DetourFunctionClass((uint8_t *)0x008FF040, &WICP_DebugView::hk_Init);
 
+	//
 	// Hook gethostbyname (IAT)
+	//
 	uintptr_t addr = (uintptr_t)&hk_gethostbyname;
 	PatchMemory(0x00BEC594, (PBYTE)&addr, sizeof(uintptr_t));
 
 	MN_NetRequester::InitializeHook();
 	MMG_CdKey::InitializeHook();
-	MR_RenderD3D10::InitializeHook();
 
 	return TRUE;
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
-	if(ul_reason_for_call != DLL_PROCESS_ATTACH)
+	if (ul_reason_for_call != DLL_PROCESS_ATTACH)
 		return TRUE;
 
 	return Wic_HookInit(hModule, ul_reason_for_call);

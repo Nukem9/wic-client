8b5c80df3b90e960bcb67f0b51b7d037b15205fd
Nukem <Nukem@outlook.com>
2021-04-20 20:02:44 -0400

    ED: Nop floating point exception functions in WICEd

--- components/wic_ed_hook/src/dllmain.cpp
+++ components/wic_ed_hook/src/dllmain.cpp
@@ -8,8 +8,8 @@ void WicED_HookInit(HMODULE hModule)
 		if (_stricmp(reinterpret_cast<const char *>(0x0077363A), "Built by alexanders/ME525 at 10:20:52 on May  5 2008 with compiler version 1400.\n") != 0)
 		{
 			//
-			// ShowboxN.exe: Kill calls to _clearfp(), _controlfp(), and _controlfp_s() to prevent Win10 floating point exceptions
-			// 
+			// ShowboxN.exe: Kill calls to _clearfp(), _controlfp(), and _controlfp_s() to prevent Win10 floating point exceptions (CRT embedded)
+			//
 			PatchMemory(0x005A2B41, { 0x31, 0xC0, 0xC3 });
 			PatchMemory(0x00619469, { 0x31, 0xC0, 0xC3 });
 			PatchMemory(0x0061B950, { 0x31, 0xC0, 0xC3 });
@@ -17,20 +17,26 @@ void WicED_HookInit(HMODULE hModule)
 		else if (_stricmp(reinterpret_cast<const char *>(0x0071874A), "Built by fredriko/ME411 at 13:14:05 on Nov 29 2007 with compiler version 1400.\n") != 0)
 		{
 			//
-			// GuiEditor.exe: Kill calls to _clearfp(), _controlfp(), and _controlfp_s() to prevent Win10 floating point exceptions
-			// 
+			// GuiEditor.exe: Kill calls to _clearfp(), _controlfp(), and _controlfp_s() to prevent Win10 floating point exceptions (CRT embedded)
+			//
 			PatchMemory(0x0065D70D, { 0x31, 0xC0, 0xC3 });
 			PatchMemory(0x0065DAE7, { 0x31, 0xC0, 0xC3 });
 			PatchMemory(0x0066F933, { 0x31, 0xC0, 0xC3 });
 		}
-		else if (_stricmp(reinterpret_cast<const char *>(0x0), "x") != 0)
+		else if (_stricmp(reinterpret_cast<const char *>(0x00ABA008), "Built by gregore/ME646 at 10:58:47 on Mar  5 2009 with compiler version 1400.\n") != 0)
 		{
 			//
-			// WicEd.exe: Kill calls to _clearfp(), _controlfp(), and _controlfp_s() to prevent Win10 floating point exceptions
-			// 
-			PatchMemory(0, { 0x31, 0xC0, 0xC3 });
-			PatchMemory(0, { 0x31, 0xC0, 0xC3 });
-			PatchMemory(0, { 0x31, 0xC0, 0xC3 });
+			// WICEd.exe: Kill calls to _clearfp(), _controlfp(), and _controlfp_s() to prevent Win10 floating point exceptions
+			//
+			auto module = reinterpret_cast<uintptr_t>(GetModuleHandleW(nullptr));
+			auto returnZeroFunction = +[]() -> int
+			{
+				return 0;
+			};
+
+			Detours::IATHook(module, "MSVCR80.dll", "_clearfp", reinterpret_cast<uintptr_t>(returnZeroFunction));
+			Detours::IATHook(module, "MSVCR80.dll", "_controlfp", reinterpret_cast<uintptr_t>(returnZeroFunction));
+			Detours::IATHook(module, "MSVCR80.dll", "_controlfp_s", reinterpret_cast<uintptr_t>(returnZeroFunction));
 		}
 	}
 	__except (EXCEPTION_EXECUTE_HANDLER)
--- components/wic_ed_hook/src/stdafx.h
+++ components/wic_ed_hook/src/stdafx.h
@@ -4,4 +4,7 @@
 #include <windows.h>
 #include <stdint.h>
 
+// Shared libraries
+#pragma comment(lib, "detours.lib")
+#include "../../shared/detours/detours/Detours.h"
 #include "../../shared/shared_utility.h"
